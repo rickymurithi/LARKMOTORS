679150dd69868346fd29f380a3cd66c8ea4c16aa
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-09-06 03:23:30 +0300

    add changes

---
+++ index.js
@@ -0,0 +1,39 @@
+//Alert function
+document.querySelector('#btn').addEventListener('click', 
+   () => alert('subscribing to LARKMOTORS'))
+
+//search list
+document.addEventListener('DOMContentLoaded', () => {
+   let form = document.querySelector('form')
+   form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      e.target.search.value
+      form.rest()
+   })
+})
+
+//function buildToDo() {
+   
+//}
+document.addEventListener("DOMContentLoaded", () => {
+   // code!
+   form = document.querySelector('form')
+   form.addEventListener('submit', (e) => {
+   e.preventDefault()
+   myTodoList(e.target.newTask.value)
+   form.reset()
+  })
+ });
+ 
+ function buildTodoList(todo) {
+   let li = document.createElement('li')
+   let btn = document.createElement('button')
+   btn.addEventListener('click', handleDelete)
+   btn.textContent = 'x'
+   li.textContent = `${todo}`
+   li.appendChild(btn)
+   document.querySelector('#tasks').appendChild(li)
+ }
+ function handleDelete (e) {
+   e.target.parentNode.remove()
+ }
