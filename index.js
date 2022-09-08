db928dc564ab3a0fe18bef748023f4d3a350bc92
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-09-08 22:41:23 +0300

    add files

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 //GET
-function get() {
-   fetch('')
+function getUSCarsData() {
+   fetch('http://localhost:3000/USCarsData')
    .then(res => res.json())
-   .then(  => )
+   .then( USCarsData => console.log(USCarsData))
 }
 //Initialize rendering.
 function initialize() {
-   get  ()
+   getUSCarsData()
 }
 initialize()
+
 
 //Alert function
 document.querySelector('#btn').addEventListener('click',
 () => alert('subscribing to LARKMOTORS'))
+
 
 //Add likes.
 let numLikes = 69
@@ -22,39 +24,57 @@ function addLike() {
    likes.innerHTML = numLikes
 }
 
+
 //search list
-document.addEventListener('DOMContentLoaded', () => {
-   let form = document.querySelector('form')
+document.addEventListener("DOMContentLoaded", () => {
+   
+   form = form.querySelector('form')
    form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      e.target.search.value
-      form.rest()
-   })
+   e.preventDefault()
+   searchCar(e.target.search.getUSCarsData)
+   form.reset()
+  })
 })
+
+function searchCar(browse) {
+   let li = document.createElement('li')
+   li.innerText = browse
+   button = document.querySelector('button')
+   button.innerText = 'x'
+   button.addEventListener('click', handleDelete)
+   button.style.color = 'orange'
+   li.appendChild(button)
+   document.querySelector('#btn').appendChild(button)
+}
+function handleDelete(e)  {
+   e.target.parentNode.remove()
+}
+
+
 
 //Add feedback.
 document.addEventListener("DOMContentLoaded", () => {
    // code!
-   form = document.querySelector('form')
+   form = comment-sec.querySelector('form')
    form.addEventListener('submit', (e) => {
    e.preventDefault()
-   myTodoList(e.target.newTask.value)
+   buildToDo(e.target.com.value)
    form.reset()
   })
- });
+})
  
- function buildTodoList(todo) {
-   let li = document.createElement('li')
-   let btn = document.createElement('button')
-   btn.addEventListener('click', handleDelete)
-   btn.textContent = 'x'
-   li.textContent = `${todo}`
-   li.appendChild(btn)
-   document.querySelector('#tasks').appendChild(li)
+function buildToDo(todo) {
+   const li = document.querySelector('li')
+   li.innerText = `${todo}`
+   button = document.createElement('button')
+   button.innerText = '&#128686;'
+   button.addEventListener('click', handleDelete)
+   button.style.color = 'yellow'
+   li.appendChild(button)
+   document.querySelector('#post-button').appendChild(button)
 }
 
 //Remove feedback.
-
 function handleDelete (e) {
    e.target.parentNode.remove()
 }
